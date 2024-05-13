4d6d3b5608ab98a961d984e633c0902e23a5a881
ShaunLWM <shaunlimweimin@gmail.com>
2024-05-13 21:47:06 +0800

    feat: remove axios

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import axios, { type AxiosResponse } from "axios";
-
 type RiskValueResponse = {
   latest_risk_value: string;
 }
 
 export class AlphaSquared {
 
+  private _apiKey: string;
+
   constructor(apiKey: string) {
-    axios.defaults.headers.common['Authorization'] = apiKey;
+    this._apiKey = apiKey;
   }
 
   async get(type: "BTC" | "ETH" = "BTC"): Promise<RiskValueResponse["latest_risk_value"]> {
     const key = type === "BTC" ? "" : `_${type}`;
-    const response: AxiosResponse<RiskValueResponse> = await axios.get(`https://alphasquared.com/wp-json/as/v1/latest-risk-value${key}`);
-    return response.data?.latest_risk_value;
+    const response = await fetch(`https://alphasquared.com/wp-json/as/v1/latest-risk-value${key}`, {
+      headers: {
+        Authorization: this._apiKey
+      }
+    });
+
+    const json = await response.json() as RiskValueResponse;
+    return json.latest_risk_value;
   }
 }
